14a9458b19496b419f87b1f5b30f4670f5806e3c
Michelle379 <michelle379123@gmail.com>
2023-12-13 20:11:37 +0800

    Initial Commit

---
+++ storygame_swift/storygame_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  storygame_swiftApp.swift
+//  storygame_swift
+//
+//  Created by 訪客使用者 on 2023/12/13.
+//
+
+import SwiftUI
+
+@main
+struct storygame_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
